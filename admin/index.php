ae18fd8e2909fb2aad158541b53241ffe2b86c02
aderahman007 <aderahman9908@gmail.com>
2019-01-20 20:57:14 -0800

    first commmit

---
+++ admin/index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admin | Dashboard</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet" />
+    <link href="../assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="../assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="../assets/css/main.min.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+</head>
+
+<body class="fixed-navbar">
+    <div class="page-wrapper">
+        <!-- START HEADER-->
+        <header class="header">
+            <div class="page-brand">
+                <a class="link" href="?page=dashboard">
+                    <span class="brand">Admin
+                        <span class="brand-tip"> BA-HI</span>
+                    </span>
+                    <span class="brand-mini">BH</span>
+                </a>
+            </div>
+            <div class="flexbox flex-1">
+                <!-- START TOP-LEFT TOOLBAR-->
+                <ul class="nav navbar-toolbar">
+                    <li>
+                        <a class="nav-link sidebar-toggler js-sidebar-toggler"><i class="ti-menu"></i></a>
+                    </li>
+                    <li>
+                        <form class="navbar-search" action="javascript:;">
+                            <div class="rel">
+                                <span class="search-icon"><i class="ti-search"></i></span>
+                                <input class="form-control" placeholder="Search here...">
+                            </div>
+                        </form>
+                    </li>
+                </ul>
+                <!-- END TOP-LEFT TOOLBAR-->
+                <!-- START TOP-RIGHT TOOLBAR-->
+                <ul class="nav navbar-toolbar">
+                    <li class="dropdown dropdown-user">
+                        <a class="nav-link dropdown-toggle link" data-toggle="dropdown">
+                            <img src="../assets/img/admin-avatar.png" />
+                            <span></span>Admin<i class="fa fa-angle-down m-l-5"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="profile.html"><i class="fa fa-user"></i>Profile</a>
+                            <a class="dropdown-item" href="profile.html"><i class="fa fa-cog"></i>Settings</a>
+                            <li class="dropdown-divider"></li>
+                            <a class="dropdown-item" href="login.html"><i class="fa fa-power-off"></i>Logout</a>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- END TOP-RIGHT TOOLBAR-->
+            </div>
+        </header>
+        <!-- END HEADER-->
+        <!-- START SIDEBAR-->
+        <nav class="page-sidebar" id="sidebar">
+            <div id="sidebar-collapse">
+                <div class="admin-block d-flex">
+                    <div>
+                        <img src="../assets/img/admin-avatar.png" width="45px" />
+                    </div>
+                    <div class="admin-info">
+                        <div class="font-strong">Ade Rahman</div><small>Administrator</small></div>
+                </div>
+                <ul class="side-menu metismenu">
+                    <li>
+                        <a class="active" href="?page=dashboard\"><i class="sidebar-item-icon fa fa-th-large"></i>
+                            <span class="nav-label">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-graduate"></i>
+                            <span class="nav-label">Guru</span><i class="fa fa-angle-left arrow"></i></a>
+                        <ul class="nav-2-level collapse">
+                            <li>
+                                <a href="#">Guru TK</a>
+                            </li>
+                            <li>
+                                <a href="#">Guru MI</a>
+                            </li>
+                            <li>
+                                <a href="#">Guru MTS</a>
+                            </li>
+                            <li>
+                                <a href="#">Guru SMAIT</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-alt"></i>
+                            <span class="nav-label">Siswa</span><i class="fa fa-angle-left arrow"></i></a>
+                        <ul class="nav-2-level collapse">
+                            <li>
+                                <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-alt"></i>
+                                    <span class="nav-label">Siswa TK</span><i class="fa fa-angle-left arrow"></i></a>
+                                <ul class="nav-2-level collapse">
+                                    <li>
+                                        <a href="#">Kelas 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-alt"></i>
+                                    <span class="nav-label">Siswa MI</span><i class="fa fa-angle-left arrow"></i></a>
+                                <ul class="nav-2-level collapse">
+                                    <li>
+                                        <a href="#">Kelas 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 4</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 5</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 6</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-alt"></i>
+                                    <span class="nav-label">Siswa MTS</span><i class="fa fa-angle-left arrow"></i></a>
+                                <ul class="nav-2-level collapse">
+                                    <li>
+                                        <a href="#">Kelas 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 3</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:;"><i class="sidebar-item-icon fas fa-user-alt"></i>
+                                    <span class="nav-label">Siswa SMAIT</span><i class="fa fa-angle-left arrow"></i></a>
+                                <ul class="nav-2-level collapse">
+                                    <li>
+                                        <a href="#">Kelas 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Kelas 3</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- END SIDEBAR-->
+        <div class="content-wrapper">
+            <!-- START PAGE CONTENT-->
+            <div class="container-fluid">
+                <div class="page-content fade-in-up">
+                     <?php
+                        if (@$_GET['page'] == 'dashboard' || @$_GET['page'] == ''){
+                            include("views/dashboard.php");
+                        }
+                     ?>
+                </div>
+            </div>        
+            <!-- END PAGE CONTENT-->
+            <footer class="page-footer">
+                <div class="font-13 text-center">2018 © <b>Admin BA-HI</b> - All rights reserved.</div>
+                <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
+            </footer>
+        </div>
+    </div>
+    <!-- BEGIN THEME CONFIG PANEL-->
+    <div class="theme-config">
+        <div class="theme-config-toggle"><i class="fa fa-cog theme-config-show"></i><i class="ti-close theme-config-close"></i></div>
+        <div class="theme-config-box">
+            <div class="text-center font-18 m-b-20">SETTINGS</div>
+            <div class="font-strong">LAYOUT OPTIONS</div>
+            <div class="check-list m-b-20 m-t-10">
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input id="_fixedNavbar" type="checkbox" checked>
+                    <span class="input-span"></span>Fixed navbar</label>
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input id="_fixedlayout" type="checkbox">
+                    <span class="input-span"></span>Fixed layout</label>
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input class="js-sidebar-toggler" type="checkbox">
+                    <span class="input-span"></span>Collapse sidebar</label>
+            </div>
+            <div class="font-strong">LAYOUT STYLE</div>
+            <div class="m-t-10">
+                <label class="ui-radio ui-radio-gray m-r-10">
+                    <input type="radio" name="layout-style" value="" checked="">
+                    <span class="input-span"></span>Fluid</label>
+                <label class="ui-radio ui-radio-gray">
+                    <input type="radio" name="layout-style" value="1">
+                    <span class="input-span"></span>Boxed</label>
+            </div>
+            <div class="m-t-10 m-b-10 font-strong">THEME COLORS</div>
+            <div class="d-flex m-b-20">
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Default">
+                    <label>
+                        <input type="radio" name="setting-theme" value="default" checked="">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-white"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Blue">
+                    <label>
+                        <input type="radio" name="setting-theme" value="blue">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-blue"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Green">
+                    <label>
+                        <input type="radio" name="setting-theme" value="green">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-green"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Purple">
+                    <label>
+                        <input type="radio" name="setting-theme" value="purple">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-purple"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Orange">
+                    <label>
+                        <input type="radio" name="setting-theme" value="orange">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-orange"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Pink">
+                    <label>
+                        <input type="radio" name="setting-theme" value="pink">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-pink"></div>
+                        <div class="color-small bg-ebony"></div>
+                    </label>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="White">
+                    <label>
+                        <input type="radio" name="setting-theme" value="white">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Blue light">
+                    <label>
+                        <input type="radio" name="setting-theme" value="blue-light">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-blue"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Green light">
+                    <label>
+                        <input type="radio" name="setting-theme" value="green-light">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-green"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Purple light">
+                    <label>
+                        <input type="radio" name="setting-theme" value="purple-light">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-purple"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Orange light">
+                    <label>
+                        <input type="radio" name="setting-theme" value="orange-light">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-orange"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+                <div class="color-skin-box" data-toggle="tooltip" data-original-title="Pink light">
+                    <label>
+                        <input type="radio" name="setting-theme" value="pink-light">
+                        <span class="color-check-icon"><i class="fa fa-check"></i></span>
+                        <div class="color bg-pink"></div>
+                        <div class="color-small bg-silver-100"></div>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END THEME CONFIG PANEL-->
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div>
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS-->
+    <script src="../assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+    <script src="../assets/vendors/chart.js/dist/Chart.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
+    <script src="../assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+    <script src="../assets/vendors/jvectormap/jquery-jvectormap-us-aea-en.js" type="text/javascript"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="../assets/js/app.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="../assets/js/scripts/dashboard_1_demo.js" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ admin/views/dashboard.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-lg-12">
+    <h1>Dashboard</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="index.html"><i class="icon-dashboard"></i> Dashboard</a></li>
+    </ol>
+    </div>
+</div><!-- /.row -->
+
+<div class="row">
+    <div class="col-lg-12">
+    <h1>Selamat Datang Admin</h1>
+    </div>
+</div>
